4a0348dc597e8ef6aa2b3bf4ee6af7976f0491fd
Toby-Fm <mail@toby-wichmann.de>
2023-11-30 20:48:43 +0100

    code überarbeitung und ansprechen von buttons

--- src/include/main.hpp
+++ src/include/main.hpp
@@ -1,30 +1,29 @@
 #include <iostream>
-#include <tchar.h>
 #include <Windows.h>
 #include <WinUser.h>
 #include <string>
-#include <CommCtrl.h>
-
-//HWND g_hInputField = NULL;
-HWND g_hButton = NULL;
+#include <CommCtrl.h> // Für Steuerelemente wie Textfelder
 
 class ButtonCreator {
 public:
-    static HWND Create(LPCWSTR buttonText, int x, int y, int width, int height, boolean id, HWND parentWnd, HINSTANCE hInstance) {
+    static HWND Create(LPCWSTR buttonText, int x, int y, int width, int height, int id, HWND hwnd, HINSTANCE hInstance) {
         return CreateWindowW(
             L"BUTTON",
             buttonText,
             WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, // Fügen Sie hier BS_DEFPUSHBUTTON hinzu
-            x, y, width, height,
-            parentWnd,
-            (HMENU)1,
+            x, 
+			y, 
+			width, 
+			height,
+            hwnd,
+            (HMENU)IntToPtr(id),
             hInstance,
             NULL
         );
     }
 };
 
-
+// Die Window Procedure-Funktion, die Nachrichten für unser Fenster verarbeitet
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	switch (msg) {
 		case WM_CLOSE: {
@@ -36,19 +35,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 			break;
 		}
 		case WM_COMMAND: {
-			OutputDebugString("WM_COMMAND received\n");
-			if (LOWORD(wParam) == 1) {
-				MessageBoxW(hwnd, L"der Button wurde geklickt!", L"Button-Klick", MB_OK);
-				break;
+			int buttonID = LOWORD(wParam);
+			switch(buttonID) {
+				case 1: {
+					MessageBoxW(hwnd, L"der Button 1 wurde geklickt!", L"Button-Klick", MB_OK);
+					break;
+				}
+				case 2: {
+					MessageBoxW(hwnd, L"der Button 2 wurde geklickt!", L"Button-Klick", MB_OK);
+					break;
+				}
 			}
 			break;
 		}
 		case WM_CREATE: {
-			OutputDebugString("WM_CREATE received\n");
-			//HWND button = ButtonCreator::Create(L"Button erzeugen", 500, 500, 150, 70, true, hwnd, (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE));
 			break;
 		}
-		break;
+		default: {
+			return DefWindowProcW(hwnd, msg, wParam, lParam);
+		}
 	}
-	return DefWindowProc(hwnd, msg, wParam, lParam);
+	return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,37 @@
 #include "include/main.hpp"
 
-int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR pCmdline, int nCmdShow) {
+// Die Hauptfunktion des Programms
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lCmdLine, int nCmdShow) {
+	WNDCLASSEX wc;
 	HWND hwnd;
-	//RegisterClass
+	MSG msg;
+
+	//Definieren des Fensterklassennamens
+	//const wchar_t g_szClassName[] = L"myWindowClass";
 	const wchar_t winClass[] = L"WindowClass";
-	
-	WNDCLASS wc;
-	wc.lpfnWndProc		= DefWindowProc;
-	wc.hInstance		= hinstance;					//Aktuelle Instanz des Programms
+
+	//Initialisieren der Fensterklassenstruktur
+	wc.cbSize = sizeof(WNDCLASSEX); // Größe der Struktur setzen
+	wc.lpfnWndProc		= WndProc;
+	wc.hInstance		= hInstance;					//Aktuelle Instanz des Programms
 	wc.lpszClassName	= "winClass";					//Name der Fensterklasse
 	wc.style			= CS_VREDRAW | CS_HREDRAW;		//Fensterstyle Definition
 	wc.hbrBackground	= (HBRUSH)(COLOR_WINDOW + 2);	//Hintergrund farbe des Fensters (1 = Weiß, 2 = Grau, 3 = Schwarz)
 	wc.hIcon			= LoadIcon(NULL, IDI_ERROR);	//Anwendungssymbol
 	wc.hCursor			= LoadCursor(NULL, IDC_ARROW);	//Mauszeigersymbol
+	wc.hIconSm			= LoadIcon(NULL, IDI_APPLICATION); //Kleines Anwendungssymbol laden
 	wc.cbWndExtra		= 0;							//Keine zusätlichen Klasseninformationen
 	wc.cbClsExtra		= 0;							//Keine zusätlichen Fentserinformationen
+	wc.lpszMenuName		= NULL;							// Kein Menü in der Titelleiste
 
-	RegisterClass(&wc);
+	// Registrieren der Fensterklasse
+	if (!RegisterClassEx(&wc)) {
+		// Wenn die Registrierung fehlschlägt, wird eine Fehlermeldung angezeigt
+		MessageBoxW(NULL, L"Fehler beim Registrieren der Fensterklasse", L"Fehler", MB_OK);
+		return 0;
+	}
 
-	//Erstellen des Hauptfensters
+	// Erstellen des Hauptfensters
 	hwnd = CreateWindowExW(
 		0,
 		L"winClass", //Name der Klasse
@@ -31,29 +44,32 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR pCmdline,
 		1080, //Höhe des Fensters
 		NULL, //Parent Window
 		NULL, //Handle für das Menü
-		hinstance, //Application Instance
+		hInstance, //Application Instance
 		NULL //Extra Data
 	);
-	if (hwnd == NULL) { //Wenn das Erstellen des Hauptfensters fehlschlägt, wird eine Fehlermeldung angezeigt
-		MessageBoxW(NULL, L"Fehler beim Registrieren der Fensterklasse", L"Fehler", MB_OK);
+	if (hwnd == NULL) {
+		// Wenn das Erstellen des Hauptfensters fehlschlägt, wird eine Fehlermeldung angezeigt
+		MessageBoxW(NULL, L"Fehler beim Erstellen des Hauptfensters", L"Fehler", MB_OK);
 		return 0;
 	}
 
-	//Button erzeugen
-	HWND button = ButtonCreator::Create(L"Button", 500, 500, 150, 70, true, hwnd, hinstance);
-	if (button == NULL) {//Wenn das Erstellen des Buttons fehlschlägt, wird eine Fehlermeldung angezeigt
+	// Hinzufügen eines Buttons zum Fenster
+	HWND button = ButtonCreator::Create(L"Button", 500, 500, 350, 150, 1, hwnd, hInstance);
+	HWND button2 = ButtonCreator::Create(L"Button2", 500,700, 350, 150, 2, hwnd, hInstance);
+	if (button == NULL) {
+		// Wenn das Erstellen des Buttons fehlschlägt, wird eine Fehlermeldung angezeigt
 		MessageBoxW(NULL, L"Fehler beim Erstellen des Buttons", L"Fehler", MB_OK);
 		return 0;
 	}
-	//Anzeigen des Hauptfensters
-	ShowWindow(hwnd, nCmdShow);
 
-	MSG msg;
-	//run the message loop
+	// Anzeigen des Hauptfensters
+	ShowWindow(hwnd, nCmdShow);
+	//UpdateWindow(hwnd);
+
+	// Nachrichtenschleife
 	while (GetMessage(&msg, NULL, 0, 0) > 0) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-
-	return 0;
+	return msg.wParam;
 }
